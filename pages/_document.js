2379ab112159cb350165d622a503e0546db63b90
Roman Jelínek <jelinek82@gmail.com>
2022-03-22 12:44:42 +0100

    GA changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript, Script } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,21 +10,19 @@ class MyDocument extends Document {
     return (
       <Html lang='cs'>
         <Head>
-          <script
-            async
-            src='https://www.googletagmanager.com/gtag/js?id=UA-48767601-12'
+          <Script
+            src='https://www.googletagmanager.com/gtag/js?id=id=UA-48767601-12'
+            strategy='afterInteractive'
           />
+          <Script id='google-analytics' strategy='afterInteractive'>
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'UA-48767601-12', { page_path: window.location.pathname });
-            `,
-            }}
-          />
+          gtag('config', 'UA-48767601-12');
+        `}
+          </Script>
         </Head>
         <body>
           <Main />
