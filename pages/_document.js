d42a3eacb3d6faccad4891023e951bc5c03518f8
Roman Jelinek <jelinek82@gmail.com>
2023-06-07 10:55:15 +0200

    Google ads added

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,16 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
     return (
-      <Html lang='cs'>
+      <Html lang="cs">
         <Head>
-          <script
+          {/* <script
             async
             src='https://www.googletagmanager.com/gtag/js?id=UA-48767601-12'
           />
@@ -24,6 +24,21 @@ class MyDocument extends Document {
               gtag('config', 'UA-48767601-12', { page_path: window.location.pathname });
             `,
             }}
+          /> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-11208729421"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11208729421');
+  `,
+            }}
           />
         </Head>
         <body>
@@ -31,8 +46,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
