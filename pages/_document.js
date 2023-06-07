fcf73733db74ac92deaed2c1a54664428078ec5c
Roman Jelinek <jelinek82@gmail.com>
2023-06-07 10:58:25 +0200

    Conversion added

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,9 @@ class MyDocument extends Document {
     return (
       <Html lang="cs">
         <Head>
-          {/* <script
+          <script
             async
-            src='https://www.googletagmanager.com/gtag/js?id=UA-48767601-12'
+            src="https://www.googletagmanager.com/gtag/js?id=UA-48767601-12"
           />
 
           <script
@@ -24,7 +24,7 @@ class MyDocument extends Document {
               gtag('config', 'UA-48767601-12', { page_path: window.location.pathname });
             `,
             }}
-          /> */}
+          />
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=AW-11208729421"
@@ -38,6 +38,13 @@ class MyDocument extends Document {
 
   gtag('config', 'AW-11208729421');
   `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  gtag('event', 'conversion', {'send_to': 'AW-11208729421/Igv7COyFyqgYEM3G3uAp'});
+`,
             }}
           />
         </Head>
